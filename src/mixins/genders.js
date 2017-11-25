6f3856c027b752e57204bf84f4926cc7a2d6aa78
APZelos <a.p.zelos@gmail.com>
2017-11-25 13:25:33 +0200

    Create Genders mixin

---
+++ src/mixins/genders.js
@@ -0,0 +1,13 @@
+const genders = [
+  { id: 0, description: 'Male', icon: 'fa-mars' },
+  { id: 1, description: 'Female', icon: 'fa-venus' },
+  { id: 2, description: 'Unisex', icon: 'fa-transgender' }
+]
+
+export default {
+  methods: {
+    getGender (id) {
+      return genders[id]
+    }
+  }
+}
